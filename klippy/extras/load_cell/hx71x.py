74d79214411c3dbc0dea9fcaba6be1d844640d17
Ella Fox <ella@fox.gal>
2026-01-29 11:49:39 +0100

    Fix config_cmd definition to accept new parameters

--- klippy/extras/load_cell/hx71x.py
+++ klippy/extras/load_cell/hx71x.py
@@ -82,8 +82,8 @@ class HX71xBase(LoadCellSensor):
         self.query_hx71x_cmd = None
         self.attach_probe_cmd = None
         mcu.add_config_cmd(
-            "config_hx71x oid=%d gain_channel=%d dout_pin=%s sclk_pin=%s"
-            % (self.oid, self.gain_channel, self.dout_pin, self.sclk_pin)
+            "config_hx71x oid=%d gain_or_sps=%d sps=%d gain_channel=%d dout_pin=%s sclk_pin=%s"
+            % (self.oid, self.gain_or_sps, self.sps_bits, self.gain_channel, self.dout_pin, self.sclk_pin)
         )
         mcu.add_config_cmd(
             "query_hx71x oid=%d rest_ticks=0" % (self.oid,), on_restart=True
